d2ad73ce81237a4f7852a51001e7dfce540d110a
Declan O’Brien <80363579+declanobrien03@users.noreply.github.com>
2026-01-21 20:48:17 -0600

    redo architecture part 3

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
+import { initDb } from "./db.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+await initDb(); // <-- wait for DB initialization before routes
 
 app.use("/", routes);
 
