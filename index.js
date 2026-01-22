56534ba6d4de2760799f86426646c3db96bb6f83
Declan O’Brien <80363579+declanobrien03@users.noreply.github.com>
2026-01-21 19:55:45 -0600

    add routes api

--- index.js
+++ index.js
@@ -19,5 +19,17 @@ app.post("/items", async (req, res) => {
   res.json({ success: true });
 });
 
+app.post("/routes", async (req, res) => {
+  db.data.routes ||= [];
+  db.data.routes.push(req.body);
+  await db.write();
+  res.json({ success: true });
+});
+
+app.get("/routes", (req, res) => {
+  res.json(db.data.routes || []);
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
